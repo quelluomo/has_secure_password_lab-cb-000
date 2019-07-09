10b3d589908242e5e818cafbc7a6b977c7514d7d
Matteo Muscas <thatman.j@gmail.com>
2019-07-09 08:33:03 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
 
   def home
     redirect_to controller: 'sessions', action: 'new' unless session[:name]
+    @user = User.name
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
