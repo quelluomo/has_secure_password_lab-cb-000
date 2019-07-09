2d2787d4f80efdf28e4867a95ee97e8d29686704
Matteo Muscas <thatman.j@gmail.com>
2019-07-09 10:04:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    user = (User.find_by(id: session[:user_id]) || User.new)
+    @user = (User.find_by(id: session[:user_id]) || User.new)
   end
 end
