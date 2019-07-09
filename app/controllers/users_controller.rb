f22a8467faf287009a5eb549a0e2dee4fe0a5570
Matteo Muscas <thatman.j@gmail.com>
2019-07-09 09:06:50 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+
+  def new 
+  end
+  
   def create
-    @user = User.create(user_params) if @user.save
-    session[:user_id] = @user.id
+    @user = User.create(user_params) 
+      if @user.save
+        session[:user_id] = @user.id
     else
       redirect_to controller: 'users', action: 'new'
     end
