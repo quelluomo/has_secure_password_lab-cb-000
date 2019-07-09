b274a430371fb265ea89f0ed3fdb8aa3a86a1bd2
Matteo Muscas <thatman.j@gmail.com>
2019-07-09 09:21:06 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(name: params[:name])
     authenticated = @user.try(:authenticate, params[:password])
     return head(:forbidden) unless authenticated
     @user = user
