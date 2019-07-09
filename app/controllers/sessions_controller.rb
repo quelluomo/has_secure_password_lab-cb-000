bc39d565a7575c642f74c3f0a4127e60222b4f93
Matteo Muscas <thatman.j@gmail.com>
2019-07-09 08:25:39 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to root_path
+  end
 end
