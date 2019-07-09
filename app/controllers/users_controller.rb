733959bd7127cd29b2b991ce5c0e6ac45045d5f4
Matteo Muscas <thatman.j@gmail.com>
2019-07-09 09:55:32 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
       if @user.save
         session[:user_id] = @user.id
-        render '/'
+        render '/users/home'
     else
       redirect_to controller: 'users', action: 'new'
     end
