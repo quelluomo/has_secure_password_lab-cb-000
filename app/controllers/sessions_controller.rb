7cfca9fb2de9abeb4468b894dfbe18b0511b9b73
Matteo Muscas <thatman.j@gmail.com>
2019-07-09 09:20:24 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:name])
-    if @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect_to '/home'
-    else
-      return head(:forbidden)
-    end
+    user = User.find_by(username: params[:username])
+    authenticated = user.try(:authenticate, params[:password])
+    return head(:forbidden) unless authenticated
+    @user = user
+    session[:user_id] = @user.id
   end
 
   def destroy
     session.delete :name
     redirect_to root_path
   end
-
-  private
-
-
 end
